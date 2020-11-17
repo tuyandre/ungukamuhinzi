d27eadce0fcf3fd6af1d846459bc09a2e8e43a74
tuyandre <36557797+tuyandre@users.noreply.github.com>
2020-11-17 09:46:50 +0200

    plugin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,4 +41,132 @@ class UserController extends Controller
 
         ], 200);
     }
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'phone' => 'required|min:10|max:10',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+
+                'Data' => 'Validation Error.',
+                'message' => $validator->errors(),
+                'Status' => 400,
+            ];
+            return response()->json($response, 400);
+        }
+        $input = $request->only('phone', 'password');
+        $jwt_token = null;
+        $phone=$request->get('phone');
+        try{
+            $users = User::where('phone','=',$phone)->where('status','=',1)->get();
+            if (!$jwt_token = JWTAuth::attempt($input)) {
+                return response()->json([
+                    'Message' => 'Invalid Credential',
+                    'Status' => 401,
+                ], 401);
+            }
+        } catch (JWTException $e){
+            return response()->json(['Error'=>'could_not_create_token','Status'=>400],400);
+
+        }
+
+        $user = array();
+        $usr = array();
+        foreach($users as $us){
+            $usr['message']='Success';
+            $usr['id']=$us->id;
+            $usr['fullname']=$us->fullname;
+            $usr['level']=$us->level;
+            $usr['token']=$jwt_token;
+
+            array_push($user,$usr);
+        }
+        $count=$users->count();
+        if($count>0){
+            return response()->json($user);
+        }else{
+            return response()->json(['Message'=>'Your acount blocked','Status'=>401],401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'token' => 'required'
+        ]);
+        if ($validator->fails()) {
+            $response = [
+                'message' => $validator->errors(),
+                'Data' => 'Validation Error.',
+                'Status' => 400,
+            ];
+            return response()->json($response, 400);
+        }
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+
+                'message' => 'User logged out successfully',
+                'Status' => 200,
+            ],200);
+        } catch (JWTException $exception) {
+            return response()->json([
+
+                'Message' => 'Sorry, the user cannot be logged out',
+                'Status' => 400,
+            ], 400);
+        }
+    }
+
+    public function getAuthUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'token' => 'required']);
+
+        if ($validator->fails()) {
+            $user = JWTAuth::authenticate($request->token);
+            $response = [
+                'message' => $validator->errors(),
+                'Data' => 'Validation Error.',
+                'Status' => 400,
+            ];
+
+        }
+        return response()->json(['user' => $user],200);
+    }
+
+
+    public function sendCode(Request $request){
+        $codes= new user();
+        $send=$codes->phone=$request->phone;
+        $usr=DB::table('users')->where('phone','=',$send)
+            ->get();
+        if($usr->count()>0){
+            $code=rand(1111,9999);
+            $nexmo=app('Nexmo\Client');
+            $nexmo->message()->send([
+                'to'=>'25'.$send,
+                'from'=>"UNGUKA MUHINZI" ,
+                'text'=>'Verify code: '.$code,
+            ]);
+            $user=DB::table('users')->where('phone','=',$send)->update(array('code'=>$code));
+            return response()->json(['Message'=>'success','Data'=>$code,'Status'=>200],200);
+        }else{
+            return response()->json(['Message'=>'Error, This phone are not available','Status'=>400],400);
+        }
+    }
+    public function viewCrops(){
+        $crops=DB::table('crops')->get();
+        $count=$crops->count();
+        if($count>0){
+            return response()->json(['Message'=>'Success','Data'=>$crops,'Data_returned'=>$count,'Status'=>200],200);
+        }else{
+            return response()->json(['Message'=>'Success','Data'=>'There is no any kind of crops you have','Status'=>200],200);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,49 @@ Route::post('/ungukamuhinzi/login/user',
     [App\Http\Controllers\admin\AdminController::class, 'loginUser'
     ])->name('frontend.loginUser');
 
+
+
+
+
+Route::post('logout', 'UserController@logout');
+Route::post('complete', 'farmer\FarmerController@store');
+Route::post('user', 'UserController@getAuthUser');
+Route::post('createFarms', 'farmer\FarmController@store');
+Route::post('viewFarms', 'farmer\FarmController@index');
+Route::post('ShowFarmer','farmer\FarmerController@show');
+Route::post('ShowFarm','farmer\FarmController@show');
+Route::post('UpdateFarmer','farmer\FarmerController@update');
+Route::post('UpdateFarms','farmer\FarmController@update');
+Route::post('AddExpense','farmer\ExpenseController@store');
+Route::post('ViewExpenses','farmer\ExpenseController@show');
+Route::post('AllFarms', 'farmer\ExpenseController@index');
+Route::post('addStock','farmer\StockController@store');
+Route::post('stock','farmer\StockController@index');
+Route::post('updateStock','farmer\StockController@update');
+Route::post('SignleStockDetails','farmer\StockController@show');
+Route::post('StockWithExpense','farmer\StockController@Profit');
+Route::post('publish','farmer\StockController@publish');
+Route::post('pending2','farmer\FarmerController@myOrders');
+Route::post('changePassword','farmer\FarmerController@changePassword');
+Route::post('checkProfit','farmer\FarmerController@checkProfit');
+Route::post('process','farmer\FarmerController@processing');
+Route::post('delivered2','farmer\FarmerController@delivered');
+Route::post('cancelled2','farmer\FarmerController@cancelled');
+Route::post('accept','farmer\FarmerController@accept');
+Route::post('decline','farmer\FarmerController@cancel');
+Route::post('markAsDelivered','farmer\FarmerController@markAsDelivered');
+Route::post('restore','farmer\FarmerController@restore');
+Route::post('changePhone','farmer\FarmerController@changePhone');
+Route::post('createCurrentCrop','farmer\ExpenseController@newCrop');
+Route::post('totalProf','farmer\StockController@allProfit');
+Route::post('feeds','farmer\StockController@feeds');
+Route::post('insideCrop','farmer\FarmController@farmsUsed');
+Route::post('insideProfile','farmer\FarmController@insideProfile');
+
+
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
